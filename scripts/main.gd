b3676c885af6cc45577ae2bf39af47aee02ed46f
mehrad1w3 <118200149+mehrad1w3@users.noreply.github.com>
2025-09-13 02:17:27 +0330

    Connect StartBlackjack button to handler
    Added a signal connection for the StartBlackjack button's 'pressed' event to the _on_start_blackjack_pressed function in _ready(). This ensures the handler is called when the button is pressed.

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,7 +4,8 @@ func _ready() -> void:
 	#on start
 	$Clicker.show()
 	$blackjack.hide()
-
+	$Clicker/StartBlackjack.connect("pressed", Callable(self, "_on_start_blackjack_pressed"))
+	
 func _on_start_blackjack_pressed():
 	var chips = $Clicker.chips   	
 		
